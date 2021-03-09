222b42dadf8c8cf4b461cda61bf3eb3511cbd72b
AlbertCos <cosmenalberto@gmail.com>
2021-03-09 01:09:47 +0100

    Take it data from website, and added to header meanu

--- covid_tracker_web/src/App.js
+++ covid_tracker_web/src/App.js
@@ -1,12 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {FormControl, MenuItem, Select} from "@material-ui/core"
 
 import './App.css';
 
 function App() {
 
-  const [countries, setCountries] = useState(['USA','Uk','Spain']);
+  const [countries, setCountries] = useState([]);
   /*https://disease.sh/v3/covid-19/countries*/
+
+  useEffect(() =>{
+    const getCountriesData = async () =>{
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) =>{
+        const countries = data.map((country)=>(
+          {
+            name:country.country,
+            value: country.countryInfo.iso2, 
+          }));
+        setCountries(countries);
+      });
+    };
+    getCountriesData()
+  },[]);
 
   return (
     <div className="app">
@@ -16,7 +32,7 @@ function App() {
         <Select variant = "outlined"value = "abc">
 
           {countries.map(country =>(
-            <MenuItem value={country}>{country}</MenuItem>
+            <MenuItem value={country.value}>{country.name}</MenuItem>
           ))}
 
           {/* <MenuItem value = "worldwide">Option1</MenuItem> */}
